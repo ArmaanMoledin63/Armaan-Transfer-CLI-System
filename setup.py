282e83758d205c22dc625d889ad0e0fc67c51906
ArmaanMoledin63 <147037852+ArmaanMoledin63@users.noreply.github.com>
2025-02-08 10:35:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="armaan-transfer",  # Pick a unique name
+    name="armaan-transfer",  
     version="0.1.0",
     packages=["quicktransfer"],
     package_dir={"": "src"},
