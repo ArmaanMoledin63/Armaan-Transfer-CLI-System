5907b3cd84fef994037a7fe47660f20d346e65f6
Armaan Moledina <147037852+ArmaanMoledin63@users.noreply.github.com>
2025-02-08 12:35:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
-
 setup(
-    name="armaan-transfer",  
+    name="armaan-transfer",
     version="0.1.0",
     packages=["quicktransfer"],
     package_dir={"": "src"},
     install_requires=["tqdm"],
     entry_points={
-        "console_scripts": ["ftransfer=quicktransfer.transfer:main"]
+        "console_scripts": ["armaan-transfer=quicktransfer.transfer:main"]
     },
 )
